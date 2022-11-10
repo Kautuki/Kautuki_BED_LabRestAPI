c8966125db7ccdaad69f17dce687a8e2eeea616c
Kautuki <80846505+Kautuki@users.noreply.github.com>
2022-11-10 18:14:47 +0530

    Add files via upload

---
+++ Kautuki_BED_LabRestAPI/collegeevent/src/main/java/com/kautuki/greatlearning/collegeevent/repository/StudentRepository.java
@@ -0,0 +1,11 @@
+package com.kautuki.greatlearning.collegeevent.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.kautuki.greatlearning.collegeevent.model.Student;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long>{
+
+}
